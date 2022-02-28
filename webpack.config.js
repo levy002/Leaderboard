100e22caaf916a71bc0d0d38e5cfbd1bffc66f60
levy002 <levy.ukwi002@gmail.com>
2022-02-28 20:15:31 +0200

    webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({
+    filename: 'index.html',
+    template: path.resolve(__dirname, './src/board.html'),
+  })],
+};
